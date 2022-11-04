b1d64bcd078c51c7d8d714041d25e005ac6974aa
Denver Conger <denverconger@gmail.com>
2022-11-03 19:27:52 -0600

    added code for autoencoder
    autoencoder will train now

--- facecropper.py
+++ facecropper.py
@@ -8,24 +8,21 @@ from autocrop import Cropper
 
 from glob import glob
 
-faces = [f for f in glob("data/uchtdorf/*") if not f.endswith("md")]
+faces = [f for f in glob("data/*") if not f.endswith("md")]
 print(f"{len(faces)} images to test with.")
 
-c = Cropper(width=512, height=512, face_percent=40)
+c = Cropper(face_percent=50)
 
 
-def save_crops(faces, cropper):
-    """Given a list on filepaths, crops and plots them."""
-    count = 0
-    for face in faces:
-        try:
-            img_array = c.crop(face)
-        except (AttributeError, TypeError):
-            pass
-        if img_array is not None:
-            img_array_rgb = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
-            cv2.imwrite(f"pics/uchtdorf/uchtdorf_face{count}.jpg", img_array_rgb)
-            print(f"saved to pics/uchtdorf/uchtdorf_face{count}.jpg")
-        count +=1
-          
-save_crops(faces, c)
+count = 0
+for face in faces:
+    face = cv2.imread(face)
+    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+    try:
+        img_array = c.crop(face)
+    except (AttributeError, TypeError):
+        pass
+    if img_array is not None:
+        cv2.imwrite(f"pics/denver/denver_face{count}.jpg", img_array)
+        print(f"saved to pics/denver/denver_face{count}.jpg")
+    count +=1
--- makephotos.py
+++ makephotos.py
@@ -1,10 +1,14 @@
 #Limited is better than nothing.
 import cv2
-vidcap = cv2.VideoCapture('videos/uchtdorf/2014-10-3040-president-dieter-f-uchtdorf-720p-eng.mp4')
+vidcap = cv2.VideoCapture('/home/denmann99/autoencoder/videos/denver/20221103_182417.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("data/uchtdorf/frame%d.jpg" % count, image)     # save frame as JPEG file      
+  image = cv2.flip(image, 0)
+
+  
+  # image = cv2.resize(image, dim)
+  cv2.imwrite(f"data/frame{count}.jpg", image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
